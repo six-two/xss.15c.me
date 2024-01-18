fa919cb7b8091866ce8166252335b612fb4a99fc
six-two <git@six-two.dev>
2024-01-18 20:40:22 +0100

    Spelling improvements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # xss.15c.me
 
-Just a simple script fpr demo purposes
+Just a simple script causing a popup for demo purposes.
 
 ## Usage
 
@@ -9,3 +9,5 @@ Just include this script in your XSS payload:
 ```html
 <script src=https://xss.15c.me></script>
 ```
+
+You can see what the script does on the [test page that just includes the script](https://xss.15c.me/test.html).
--- xss.js
+++ xss.js
@@ -1,8 +1,8 @@
 const msg = `External demo script loaded from xss.15c.me\n\nCurrent time: ${Date()}\nCurrent URL: ${location.href}`;
 console.error(msg);
 
-// Esc/Cancel -> Do nothing
-// Enter/OK   -> Pause the debugger so that you can trace the injection point
+// Escape key / Cancel button -> Do nothing
+// Enter key  / OK button     -> Pause the debugger so that you can trace the injection point
 if (confirm(msg)) {
   console.log("OK pressed, pausing debugger");
   debugger;
